e324b61a3098623608628fea7538eaf70770fc8c
felipe.dias <felipe.dias@creditas.com.br>
2020-09-24 21:14:46 -0300

    - config refactoring

--- superset_config.py
+++ superset_config.py
@@ -1,31 +1,25 @@
 import os
-# ---------------------------------------------------------
 # Superset specific config
-# ---------------------------------------------------------
-# ROW_LIMIT = 5000
-SUPERSET_WORKERS = 4  # for it to work in heroku basic/hobby dynos increase as you like
-SUPERSET_WEBSERVER_PORT = os.environ['PORT']
-# ---------------------------------------------------------
-MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY')
+ROW_LIMIT = 5000
 
-# ---------------------------------------------------------
+SUPERSET_WEBSERVER_PORT = 8088
+
 # Flask App Builder configuration
-# ---------------------------------------------------------
 # Your App secret key
 SECRET_KEY = os.urandom(24)
 
 # The SQLAlchemy connection string to your database backend
 # This connection defines the path to the database that stores your
-# Superset metadata (slices, connections, tables, dashboards, ...).
+# superset metadata (slices, connections, tables, dashboards, ...).
 # Note that the connection information to connect to the datasources
 # you want to explore are managed directly in the web UI
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
+# SQLALCHEMY_DATABASE_URI = 'sqlite:////path/to/superset.db'
 # Flask-WTF flag for CSRF
-WTF_CSRF_ENABLED = CSRF_ENABLED = True
+WTF_CSRF_ENABLED = True
+# Add endpoints that need to be exempt from CSRF protection
+WTF_CSRF_EXEMPT_LIST = []
+# A CSRF token that expires in 1 year
+WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
 
-# use inserted X-Forwarded-For/X-Forwarded-Proto headers
-ENABLE_PROXY_FIX = True
-SQLLAB_ASYNC_TIME_LIMIT_SEC = 300
-SQLLAB_TIMEOUT = 300
-SUPERSET_WEBSERVER_TIMEOUT = 300
+# Set this API key to enable Mapbox visualizations
+MAPBOX_API_KEY = ''
