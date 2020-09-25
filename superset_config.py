1d9be576e79e12fe88852de447c73cd3592e1f01
felipe.dias <felipe.dias@creditas.com.br>
2020-09-25 17:48:17 -0300

    - disabling WTF_CSRF_ENABLED

--- superset_config.py
+++ superset_config.py
@@ -17,7 +17,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:////app/.superset/superset.db'
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = False
 # Add endpoints that need to be exempt from CSRF protection
-WTF_CSRF_EXEMPT_LIST = ['heroku.com', 'marielle-superset.herokuapp.com']
+WTF_CSRF_EXEMPT_LIST = []
 # A CSRF token that expires in 1 year
 WTF_CSRF_TIME_LIMIT = 3600
 WTF_CSRF_SECRET_KEY = TOKEN
